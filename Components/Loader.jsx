ae4ffc1d5e548ac31758a8bbe46dfa2e1f3fd9e2
bassamahager <bassam@kaskade.finance>
2024-10-09 08:34:19 +0400

    tbu 2

---
+++ Components/Loader.jsx
@@ -0,0 +1,16 @@
+const Loader = () => {
+  return (
+    <div className="new-loader-wrapper">
+      <div className="new-wrapper">
+        <div className="new-circle"></div>
+        <div className="new-circle"></div>
+        <div className="new-circle"></div>
+        <div className="new-shadow"></div>
+        <div className="new-shadow"></div>
+        <div className="new-shadow "></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
